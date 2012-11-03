919aab5bd6a50b4fa34e8ed3738ce57c2f012ff0
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2012-11-03 15:32:34 -0700

    merge issue

--- Shared/js/audioScrobbler.js
+++ Shared/js/audioScrobbler.js
@@ -1,51 +1,3 @@
-<<<<<<< HEAD:Website/scripts/audioScrobbler.js
-define(function(){
-    'use strict';
-    var url = 'http://ws.audioscrobbler.com/2.0/';
-    //[Meo] TODO: Should we be exposing this API key to github?
-    var apiKey = '29c1ce9127061d03c0770b857b3cb741';
-    var format = 'json';
-    var dataType = 'json';
-
-    return {
-        getData: function(songTitle, callback){
-            $.ajax({
-                url: url,
-                data: {
-                    "method": "track.search",
-                    "track": songTitle,
-                    "api_key": apiKey,
-                    "format": format
-                },
-                dataType: dataType,
-                success: callback 
-            });
-        },
-
-        getAlbum: function(title, artist, callback){
-            $.ajax({
-                url: url,
-                data: {
-                    "method": "track.getInfo",
-                    "track": title,
-                    "artist": artist,
-                    "format": format,
-                    "api_key": apiKey
-                },
-                dataType: dataType,
-                success: callback
-            });
-        },
-        getAlbumInfo: function(data, callback) {
-            $.ajax({
-                url: url,
-                data: data,
-                dataType: dataType,
-                success: callback
-            })
-        }
-    };
-=======
 define(function () {
     'use strict';
     var url = 'http://ws.audioscrobbler.com/2.0/';
@@ -96,5 +48,4 @@ define(function () {
             });
         }
     };
->>>>>>> Reworked yt_helper to decouple it, still in the process of refactoring lots.:Shared/js/audioScrobbler.js
 });
