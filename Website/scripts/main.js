694ad1e07ad3f7c72e5bf76fff3366aba0e93225
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2012-11-05 12:49:45 -0800

    Bug fixes with how require declarations are being done

--- Website/scripts/main.js
+++ Website/scripts/main.js
@@ -1,16 +1,18 @@
-require(['jquery', 'helpers', 'strings'], function () {
+require(['jquery', 'helpers', 'strings', 'underscore'], function () {
     'use strict';
     $(function () {
-        $.each($("[data-lang-id]"), function(key, node) {
-            $(node).html(strings[$(node).attr("data-lang-id")][language])
-        })
-        
-        require(['bindings', 'recognition', 'contextmaster', 'underscore']);
-        define(['fblogin'], function (fblogin) {
-            var loginButton = $('#login-button');
-            var loginString = $('#login-string');
-            var accountArea = $('#account-area');
-            fblogin.createLoginButton(loginButton, loginString, accountArea);
+        $.each($("[data-lang-id]"), function (key, node) {
+            $(node).html(strings[$(node).attr("data-lang-id")][language]);
         });
+
+        require(['bindings', 'recognition', 'contextmaster'], function () {
+            define(['fblogin'], function (fblogin) {
+                var loginButton = $('#login-button');
+                var loginString = $('#login-string');
+                var accountArea = $('#account-area');
+                fblogin.createLoginButton(loginButton, loginString, accountArea);
+            });
+        });
+
     });
 });
